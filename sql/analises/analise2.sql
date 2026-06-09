d60ea8fca244c78fb599321a69c8e7bee0648841
heitorbnobrega <heitorbrnobrega@gmail.com>
2026-06-08 21:56:18 -0300

    analise2.sql
    Added SQL query to calculate average monthly hospital beds by state.

--- sql/analises/analise2.sql
+++ sql/analises/analise2.sql
@@ -1,2 +1,10 @@
--- Arquivo com consulta de SQL 
--- Trocar o nome do arquivo quando decidir a consulta
+-- Média mensal de leitos existentes dos municípios de cada UF
+FROM (
+    SELECT e.uf, t.mes, t.ano, SUM(f.leitos_existentes) AS leitos_municipio
+    FROM elt.fato_leitos_mensais f
+    JOIN elt.dim_estabelecimento_saude e ON e.id_estabelecimento = f.id_estabelecimento
+    JOIN elt.dim_tempo t ON t.id_tempo = f.id_tempo
+    GROUP BY e.uf, t.mes, t.ano
+) total_por_municipio
+GROUP BY uf
+ORDER BY AVG(leitos_municipio) DESC;
